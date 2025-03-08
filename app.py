b7456853d69e0b322131715e513e5f996014d3bc
Avraham Edelstein <avraham.edelstein@odysii.com>
2025-03-08 22:15:30 +0200

    Avraham add

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, World Avraham"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use Heroku's port
